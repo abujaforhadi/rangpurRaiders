0fdaa630a8f4a88e674239e24e4549b7136fbdec
abujaforhadi <abujaforhadi@gmail.com>
2024-10-25 03:21:31 +0600

    complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,23 +6,25 @@ function Hero() {
   return (
     <div>
       <div
-        className="hero  rounded-lg bg-cover bg-no-repeat"  style={{
-            backgroundImage: `url(${pic1})`, 
-          }}
-        
+        className="hero  rounded-lg bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url(${pic1})`,
+        }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <figure className="">
-                <img src={pic} alt="" srcset="" />
-            </figure>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+        <div className="hero-content text-center">
+          <div className="max-w-lg">
+            <img className="mx-auto" src={pic} alt="Rangpur Riders Logo" />
+            <p className="py-6 w-full text-white text-xl">
+              The Rangpur Riders are a professional cricket team based in
+              Rangpur City, Rangpur. The Riders compete in Bangladesh Premier
+              League (BPL), the premier cricket league in Bangladesh.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <p className="text-[#FFFFFFB3] pb-4 font-inter">
+              Beyond Boundaries Beyond Limits
+            </p>
+            <button className="btn btn-outline btn-warning">
+              Claim Free Credit
+            </button>
           </div>
         </div>
       </div>
--- src/Components/MainSection.jsx
+++ src/Components/MainSection.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Hero from './Hero'
+import React from "react";
+import PropTypes from "prop-types";
+import Hero from "./Hero";
 
 function MainSection() {
   return (
     <div>
-        <h1>main</h1>
-        <Hero></Hero>
-
+      <h1>main</h1>
+      <Hero></Hero>
+      <div className="flex justify-between py-5">
+        <h3 className="font-bold text-xl">Available Players</h3>
+        <div  className="flex gap-2">
+          <button className="btn btn-outline font-bold text-xs">Available</button>
+          <button className="btn btn-outline font-bold text-xs">Selected (0)</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-MainSection.propTypes = {}
+MainSection.propTypes = {};
 
-export default MainSection
+export default MainSection;
