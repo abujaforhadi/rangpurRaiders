0574297d54cbceb5ab2e3d63df9cd24a4ef2a28b
abujaforhadi <abujaforhadi@gmail.com>
2024-10-25 04:09:12 +0600

    update player card

--- src/Components/Available.jsx
+++ src/Components/Available.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Player from './Player';
 
 function Available() {
+    const [player,setPlayer]=useState([]);
+    useEffect(() => {
+        fetch("/players.json")
+          .then((response) => response.json())
+          .then((data) => {
+            const shuffledPlayers = data.players.sort(() => 0.5 - Math.random()); 
+            const limitedPlayers = shuffledPlayers.slice(0, 6);
+            setPlayer(limitedPlayers);
+          });
+      }, []);
+
   return (
+   
     <div>
         <h3 className="font-bold text-xl text-center">Available Players </h3>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            {
+                player.map(player=><Player player={player}></Player>)
+            }
+        </div>
     </div>
   )
 }
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
 
-function Player() {
+function Player({ player }) {
+  console.log(player);
+  const { name, image, country, battingType, biddingPrice, role, bowlingType } =
+    player;
   return (
-    <div>Player</div>
-  )
+    <div>
+      <div className="card  shadow-xl">
+        <figure className="px-5 pt-8">
+          <img src={image} alt={name} className="w-64 h-64 rounded-xl" />
+        </figure>
+        <div className="p-4">
+          <h2 className=" text-xl font-semibold">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 inline-block pr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            {name}
+          </h2>
+          <div className="flex justify-between py-3">
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6 inline-block"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
+                />
+              </svg>{" "}
+              {country}
+            </p>
+            <p className="bg-gray-300 py-1 px-2 rounded-lg">{role}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Player
+export default Player;
